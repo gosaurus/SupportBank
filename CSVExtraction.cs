28fe0c52c7ed1537da8b12fe771384f608e198f1
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-02-25 16:07:50 +0000

    Started Transaction class and CSVExtraction logic

---
+++ CSVExtraction.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.Common;
+using System.Diagnostics.Metrics;
+using System.Linq.Expressions;
+
+class Extraction
+{
+    public static void CSVExtraction()
+    {
+        string filePath =
+        @".\Transactions2014.csv";
+        StreamReader reader = null;
+        if (File.Exists(filePath))
+        {
+            reader = new StreamReader(File.OpenRead(filePath));
+            List<string> transactions2014List = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                transactions2014List.Add(line);
+            }
+            // foreach (var transactionRawString in transactions2014List)
+            // {
+            //     transactionRawString.Split(',');
+            //     Transactions.Transaction(transactionRawString);
+
+            //     var values = line.Split(',');
+            // }
+            for (var count = 1; count < 5; count++)
+            {
+                int id = count;
+                string transactionRawString = transactions2014List[count];
+                Console.WriteLine(transactionRawString, transactions2014List[count]);
+                string[] splitString = transactionRawString.Split(','); 
+                foreach (var element in splitString) //why do we need to do foreach and console.writeline(element) to see each thing in splitString
+                {
+                    Console.WriteLine(element);
+                }
+                // Transactions.Transaction(id,transactionSplitString);
+            }
+        }
+        else 
+        {
+            Console.WriteLine("File doesn't exist");
+        }
+    Console.ReadLine();
+    }
+
+
+}
+
--- Program.cs
+++ Program.cs
@@ -1,28 +1,7 @@
-﻿class Program
+class Program
 {
     public static void Main()
     {
-        string filePath =
-        @".\Transactions2014.csv";
-        StreamReader reader = null;
-        if (File.Exists(filePath))
-        {
-            reader = new StreamReader(File.OpenRead(filePath));
-            List<string> listA = new List<string>();
-            while (!reader.EndOfStream)
-            {
-                var line = reader.ReadLine();
-                listA.Add(line);
-            }
-            foreach (var transaction in listA)
-            {
-                Console.WriteLine(transaction);
-            }
-        } 
-        else 
-        {
-            Console.WriteLine("File doesn't exist");
-        }
-    Console.ReadLine();
+        Extraction.CSVExtraction();
     }
 }
