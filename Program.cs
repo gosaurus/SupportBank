2597a4e6a6b531280795dc400edfa13d81718abb
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-02-28 11:17:29 +0000

    Started creating user-transaction dictionary

--- Program.cs
+++ Program.cs
@@ -9,32 +9,63 @@ class Program
 
         var transactionsList = Extraction.MakeTransactionListObjects(rawTransactionsList);
 
-        List<string> UniqueUserList = [];
-        
+        Dictionary<string,List<Transaction>> usersDictionary = 
+            new Dictionary<string, List<Transaction>>();
+
+
         foreach(var transaction in transactionsList)
         {
-            if (!UniqueUserList.Contains(transaction.PaidFrom))
+            if (!usersDictionary.ContainsKey(transaction.PaidFrom))
             {
-                UniqueUserList.Add(transaction.PaidFrom);
+                List<Transaction> uniqueUserTransactionList = [];
+                usersDictionary.Add(transaction.PaidFrom, uniqueUserTransactionList);
+                uniqueUserTransactionList.Add(transaction);
             }
-            if (!UniqueUserList.Contains(transaction.PaidTo))
+            else if (!usersDictionary.ContainsKey(transaction.PaidTo))
             {
-                UniqueUserList.Add(transaction.PaidTo);
+                List<Transaction> uniqueUserTransactionList = [];
+                usersDictionary.Add(transaction.PaidTo, uniqueUserTransactionList);
+                uniqueUserTransactionList.Add(transaction);
             }
-        }  
-
-        List<User> AllUsersList = [];
-
-        foreach(var uniqueUser in UniqueUserList)
-        {
-            var user = new User(uniqueUser);
-
-            AllUsersList.Add(user);
-            Console.WriteLine(user);
-            user.getTransactionsPaidFrom(transactionsList);
-            user.getTransactionsPaidTo(transactionsList);
+            else
+            {
+                
+                var uniqueUserTransactionList = usersDictionary[transaction.PaidFrom];
+                uniqueUserTransactionList.Add(transaction);
+            }
         }
-        
+    }
 
+    public static bool isExistingUser (
+        string name, Dictionary<string,
+        List<Transaction>> usersDictionary
+    )
+    {
+        return usersDictionary.ContainsKey(name);
     }
 }
+        // List<string> UniqueUserList = [];
+        
+        // foreach(var transaction in transactionsList)
+        // {
+        //     if (!UniqueUserList.Contains(transaction.PaidFrom))
+        //     {
+        //         UniqueUserList.Add(transaction.PaidFrom);
+        //     }
+        //     if (!UniqueUserList.Contains(transaction.PaidTo))
+        //     {
+        //         UniqueUserList.Add(transaction.PaidTo);
+        //     }
+        // }  
+
+        // List<User> AllUsersList = [];
+
+        // foreach(var uniqueUser in UniqueUserList)
+        // {
+        //     var user = new User(uniqueUser);
+
+        //     AllUsersList.Add(user);
+        //     Console.WriteLine(user);
+        //     user.getTransactionsPaidFrom(transactionsList);
+        //     user.getTransactionsPaidTo(transactionsList);
+        // }
